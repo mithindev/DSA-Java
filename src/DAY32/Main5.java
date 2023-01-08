3d3ab75dda7f28608e2bd9a9e779512179c75ca4
mithindev <mithindev1@gmail.com>
2023-01-08 10:42:42 +0530

    updated

---
+++ src/DAY32/Main5.java
@@ -0,0 +1,37 @@
+package DAY32;
+
+import java.util.ArrayList;
+
+public class Main5 {
+    public static void main(String[] args) {
+
+    }
+    // We need to obtain all the subsets of the given String.
+
+    static void subSeq (String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subSeq(p + ch, up.substring(1));
+        subSeq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subSeqReturn (String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+            char ch = up.charAt(0);
+
+        ArrayList<String> left = subSeqReturn (p + ch, up.substring(1));
+        ArrayList<String> right = subSeqReturn (p , up.substring(1));
+        left.addAll(right);
+        return left;
+    }
+    
+}
