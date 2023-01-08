5e41887a1466a78289c25ff3fceb917b37508438
mithindev <mithindev1@gmail.com>
2023-01-08 10:46:45 +0530

    updated

--- src/DAY32/Main5.java
+++ src/DAY32/Main5.java
@@ -33,5 +33,22 @@ public class Main5 {
         left.addAll(right);
         return left;
     }
-    
+
+    static ArrayList<String> subSeqAscii (String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> first = subSeqAscii(p, up.substring(1));
+        ArrayList<String> second = subSeqAscii(p + (ch+0), up.substring(1));
+        ArrayList<String> third = subSeqAscii(p + ch, up.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+        return first;
+    }
 }
